6e161b8d4e1fca9aac543509b10a2d05be8ae055
benefraimov <ben.a254@gmail.com>
2024-06-26 20:50:12 +0300

    added style and script files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Example of DOM manipulation
+document.addEventListener('DOMContentLoaded', function() {
+    const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+
+    // headings.forEach(function(heading) {
+    //     heading.addEventListener('mouseover', function() {
+    //         this.style.fontSize = '2em';
+    //     });
+
+    //     heading.addEventListener('mouseout', function() {
+    //         this.style.fontSize = '1.5em';
+    //     });
+    // });
+});
